7d5ddace16f2cf3da8af108af8192ee8a9d51b58
Muhammad Haram <haramh643@gmail.com>
2026-01-26 11:05:55 -0800

    commit

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # print("Heyyyyy")
+
+
+# Numbers
+a =1 #integer
+a = 1.3 #float
+a = 22/7 #float
+a = 12j #complex
+# print(type(a))
+
+
+# String
+st = "Aasjdjsdn$5&*)"
+# print(type(st))
+
+# Boolean
+t = True
+f = False
+
+
+# indexing
+ind = "python"
+# print(ind[1])
+
+
+# string slicing
+sl = "python language"
+
+print(sl[0:7:1])
+print(sl[7::1])
