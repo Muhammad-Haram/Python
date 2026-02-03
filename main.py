be0c60b5792126b9dc01d70c3fcb69fcdb9beb98
Muhammad Haram <haramh643@gmail.com>
2026-02-03 11:05:42 -0800

    commit

--- main.py
+++ main.py
@@ -26,19 +26,19 @@ ind = "python"
 # string slicing
 sl = "python language"
 
-print(sl[0:6:1])
+# print(sl[0:6:1])
 # print(sl[7::])
 
 
 # Type convertion
 con = 12
 con = float(con)
-print(type(con))
+# print(type(con))
 
 bol = -1
 bol = bool(bol)
-print(type(bol))
-print(bol)
+# print(type(bol))
+# print(bol)
 
 
 # formated string / f-string 
@@ -46,11 +46,49 @@ print(bol)
 name = "haram"
 age = 20
 
-print("my name is ",name, "and my age is ", age) #normal string
-print(f"my name is {name} and my age is {age}") #formated string
+# print("my name is ",name, "and my age is ", age) #normal string
+# print(f"my name is {name} and my age is {age}") #formated string
 
 # input 
 
-input_age = int(input("what is your age"))
-print(input_age)
-print(type(input_age))
+# input_age = int(input("what is your age"))
+# print(input_age)
+# print(type(input_age))
+
+
+# operators
+
+# arthmatic operators
+
+num1 = 20.5
+num2 = 20
+
+# print(num1 + num2) #add 24
+# print(num1 - num2) #sub 16
+# print(num1 * num2) #multiplication 80
+# print(num1 / num2) #division 5.0
+# print(num1 // num2) #floor division 5 "remove decimal values"
+# print(num2 ** 3) #Exponentiation "power"
+# print(32 % 5) #modulus
+
+#assignment operator
+
+num3 = 20
+# num3 = num3 + 40
+num3 += 40
+num3 -= 20
+# print(num3)
+
+
+# comparsion operator
+# < , > , >= , <= , == , !=
+
+# print(num1 == num2)
+# print(num1 != num2)
+print(45 > 35)
+print(35 <= 35)
+
+# logical operator 
+
+print(12 < 15 and 12 < 14 or 12 < 14 )
+
